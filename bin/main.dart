3a1f02bfff937d45717b3be41d1bbada44456e78
femalemonkeyman <joeymacdonald55@gmail.com>
2023-08-22 14:10:20 -0400

    Ah

--- bin/main.dart
+++ bin/main.dart
@@ -2,17 +2,14 @@ import 'dart:io';
 import 'dart:isolate';
 
 import 'package:hotreloader/hotreloader.dart';
+import 'package:roe_server/src/login.dart';
 
 void main() async {
   final HotReloader reloader = await HotReloader.create();
   for (var i = 1; i < 12; i++) {
     await Isolate.spawn(
       (_) async => await ServerSocket.bind('::', 8080, shared: true)
-        ..listen((event) {
-          event.listen((final input) {
-            event.write('${Isolate.current.debugName} > ');
-          });
-        }),
+        ..listen(login),
       [i],
       debugName: i.toString(),
     );
