3874950467375c242c8729615aea84043ab3766a
femalemonkeyman <joeymacdonald55@gmail.com>
2023-08-29 14:19:10 -0400

    I am trying

--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,35 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:isolate';
-
-import 'package:hotreloader/hotreloader.dart';
-import 'package:roe_server/login.dart';
+import 'package:isar/isar.dart';
+import 'package:roe_server/src/models/account.dart';
+import 'package:roe_server/src/models/character.dart';
 
 Future<void> main(List<String> args) async {
-  final HotReloader reloader = await HotReloader.create();
-  for (var i = 1; i < 12; i++) {
-    await Isolate.spawn(
-      (_) async => await ServerSocket.bind('::', 8080, shared: true)
-        ..listen(login),
-      [i],
-      debugName: i.toString(),
-    );
-  }
+  await Isar.initialize('${Directory.current.path}/libisar.so');
+  Isar.open(
+      schemas: [AccountSchema], directory: Directory.current.path, name: 'roe');
+  final List<Isolate> threads = List.unmodifiable(
+    [
+      for (int i = 1; i <= 12; i++)
+        await Isolate.spawn(execute, [], debugName: 'Thread $i'),
+    ],
+  );
   while (true) {}
+}
+
+void execute(List args) async {
+  await Isar.initialize('${Directory.current.path}/libisar.so');
+  final Isar isar = Isar.get(schemas: [AccountSchema], name: 'roe');
+  final socket = await ServerSocket.bind('::', 8080, shared: true);
+  socket.listen((event) {
+    event.writeln('Welcome, please login or create an account.');
+    event.write('Please enter your username > ');
+    event.listen((message) {
+      print(message);
+      event.write(' > ');
+    });
+  }, onError: (error) {
+    print(error);
+  });
 }
